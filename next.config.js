3fdb445fc143547c796a2f5d9a278eaf9eb79b6c
adedejiosvaldo <adedejiosvaldo@gmail.com>
2025-05-24 23:42:34 +0100

    Feature: Added dashboard

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ui-avatars.com",
+        port: "",
+        pathname: "/api/**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
