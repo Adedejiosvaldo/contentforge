eb51c1c364f754e7c2ba747001ee3a0032ce44c1
adedejiosvaldo <adedejiosvaldo@gmail.com>
2025-06-20 18:42:14 +0100

    Feat: Added analytics mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
         <ThemeProvider>
           <Providers>{children}</Providers>
         </ThemeProvider>
-        <Analytics />
+        <Analytics mode="production" />
       </body>
     </html>
   );
